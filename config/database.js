4964f478c552cbb60d2d2cc187fd58d30bee5616
Johnfavour <favourjohn@gmail.com>
2024-07-07 23:39:27 +0100

    unit testing runs successfully

--- config/database.js
+++ config/database.js
@@ -1,27 +1,3 @@
-// require('dotenv').config();
-// const { Sequelize } = require("sequelize");
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres'
-//   }
-// };
-
-// require('dotenv').config();
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   dialect: 'postgres',
-// });
-
-// module.exports = sequelize;
-
-
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../index');
 const { sequelize } = require('../models');
 
 
