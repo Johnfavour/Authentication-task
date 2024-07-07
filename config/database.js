e9db0e241bad20cac38f2c658f254ad19ba050f4
Johnfavour <favourjohn@gmail.com>
2024-07-08 00:17:40 +0100

    unit testing runs successfully

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  
+    }
+  }
 });
 
-module.exports = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-};
+module.exports = sequelize;
