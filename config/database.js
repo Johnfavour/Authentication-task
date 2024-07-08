3b00d4bbc9d0dc295a279ef7c13b717bb60dbf3a
Johnfavour <favourjohn@gmail.com>
2024-07-08 10:44:59 +0100

    configuring vercel database

--- config/database.js
+++ config/database.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false  // Adjust based on your PostgreSQL server setup
+    }
+  },
+  logging: false // Disable logging; default: console.log
 });
 
-module.exports = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-};
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to PostgreSQL database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
+module.exports = sequelize;
