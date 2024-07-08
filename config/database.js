cf3c7b2dbd0d731c9dc386364a80599bcbffb1f6
Johnfavour <favourjohn@gmail.com>
2024-07-08 16:59:32 +0100

    unit testing

--- config/database.js
+++ config/database.js
@@ -1,3 +1,59 @@
+// require('dotenv').config();
+// const { Pool } = require('pg');
+// const { Sequelize } = require('sequelize');
+
+// const connectionString = process.env.POSTGRES_URL;
+
+// console.log("Initializing Sequelize with:", connectionString); // Debug statement
+
+// const adminPool = new Pool({
+//     connectionString: connectionString,
+// });
+
+// const databaseName = new URL(connectionString).pathname.split("/")[1].replace('%2F', '');
+
+// const createDatabaseIfNotExists = async () => {
+//     const client = await adminPool.connect();
+//     try {
+//         const result = await client.query(`SELECT 1 FROM pg_database WHERE datname = $1`, [databaseName]);
+//         if (result.rowCount === 0) {
+//             await client.query(`CREATE DATABASE ${databaseName}`);
+//             console.log(`Database ${databaseName} created successfully`);
+//         } else {
+//             console.log(`Database ${databaseName} already exists`);
+//         }
+//     } catch (error) {
+//         console.error("Error checking/creating database:", error);
+//         throw error;
+//     } finally {
+//         client.release();
+//     }
+// };
+
+// const sequelize = new Sequelize(connectionString, {
+//     dialect: 'postgres', // Ensure the dialect is explicitly set to 'postgres'
+//     logging: false,
+// });
+
+// const dbConnected = new Promise(async (resolve, reject) => {
+//     try {
+//         await createDatabaseIfNotExists();
+//         await sequelize.authenticate();
+//         console.log("Connected to database");
+//         resolve();
+//     } catch (error) {
+//         console.error("Failed to connect to database:", error);
+//         reject(error);
+//     }
+// });
+
+// module.exports = {
+//     sequelize,
+//     dbConnected
+// };
+
+
+
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const { sequelize, dbConnected } = require('./config/database');
+// const authRoutes = require('./routes/auth');
+// const organisationRoutes = require('./routes/organisation');
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// app.use('/auth', authRoutes);
+// app.use('/api/organisations', organisationRoutes);
+
+// const port = process.env.PORT || 5000;
+
+// sequelize.sync()
+//     .then(() => {
+//         console.log("Database & tables created!");
+//     })
+//     .catch((error) => {
+//         console.error("Unable to sync database:", error);
+//     });
+
+// dbConnected
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log(`Server is running on port ${port}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.error("Server failed to start due to database connection error:", error);
+//         process.exit(1);
+//     });
+
+// module.exports = app;
+
+
+
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
